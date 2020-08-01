f08e031271876f4e48a41bdd68c60e03e9041b3a
alan840706 <58114247+alan840706@users.noreply.github.com>
2020-08-02 01:51:09 +0800

    Update predict.py

--- darkflow/net/yolov2/predict.py
+++ darkflow/net/yolov2/predict.py
@@ -50,7 +50,10 @@ def postprocess(self, net_out, im, save = True):
 		if self.FLAGS.json:
 			resultsForJSON.append({"label": mess, "confidence": float('%.2f' % confidence), "topleft": {"x": left, "y": top}, "bottomright": {"x": right, "y": bot}})
 			continue
-
+		print("left",left)
+		print("top",top)
+		print("right",right)
+		print("bot",bot)
 		cv2.rectangle(imgcv,
 			(left, top), (right, bot),
 			colors[max_indx], thick)
