8c258fbbc0b6a723e2ae866acfd320e5749f70c9
alan840706 <58114247+alan840706@users.noreply.github.com>
2020-08-02 22:19:33 +0800

    Update predict.py

--- darkflow/net/yolov2/predict.py
+++ darkflow/net/yolov2/predict.py
@@ -44,9 +44,8 @@ def postprocess(self, net_out, im, save = True):
 	else: imgcv = im
 	h, w, _ = imgcv.shape
 	f = open(answer_path)
-	temp = np.array([])
-	for line in f:
-		context = line.split(' ')
+	for i in range(len(f)):
+		context = f[i].split(' ')
 		print(context[1],"   ",context[2],"   ",context[3],"   ",context[4])
 		x_plot = np.float64(context[1]) *320
 		y_plot = np.float64(context[2]) *224
@@ -56,8 +55,12 @@ def postprocess(self, net_out, im, save = True):
 		gt_top = y_plot-h_long
 		gt_right = x_plot+w_long
 		gt_bot = y_plot+h_long
-		buff = [[gt_left,gt_top,gt_right,gt_bot]]
-		temp = np.append(temp,buff,axis=0)
+		buff = [gt_left,gt_top,gt_right,gt_bot]
+		
+		if (i==0):
+			temp = np.array([context[1],context[2],context[3],context[4]])
+		else:
+			temp = np.append(temp,buff)
 	print("temp",len(temp))
 	resultsForJSON = []
 	for b in boxes:
