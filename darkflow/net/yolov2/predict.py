4a4ba62c5100c9809bac220a150f4c9c26d352a3
alan840706 <58114247+alan840706@users.noreply.github.com>
2020-08-02 18:43:38 +0800

    Update predict.py

--- darkflow/net/yolov2/predict.py
+++ darkflow/net/yolov2/predict.py
@@ -39,7 +39,8 @@ def postprocess(self, net_out, im, save = True):
 	if type(im) is not np.ndarray:
 		imgcv = cv2.imread(im)
 		print("im:",im)
-		answer_path = answer_path+os.path.splitext(im)[0]+".txt"
+		str = im.split('/')
+		answer_path = answer_path+os.path.splitext(str[2])[0]+".txt"
 	else: imgcv = im
 	h, w, _ = imgcv.shape
 	f = open(answer_path)
