c5bef02a5435a9dcbe6f8072d7b61d9b4868811b
alan840706 <58114247+alan840706@users.noreply.github.com>
2020-11-27 16:50:29 +0800

    Update flow.py

--- darkflow/net/flow.py
+++ darkflow/net/flow.py
@@ -136,7 +136,7 @@ def predict(self):
         feed_dict = {self.inp : np.concatenate(inp_feed, 0)}    
         self.say('Forwarding {} inputs ...'.format(len(inp_feed)))
         start = time.time()
-        print("1: ",inp_feed)
+        print("1: ",inp_feed.shape)
         out = self.sess.run(self.out, feed_dict)
         #import cv2
 
