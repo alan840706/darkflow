232e19310e889f24438d0154251d9e7cbe95fde6
alan840706 <58114247+alan840706@users.noreply.github.com>
2021-01-20 21:43:52 +0800

    Update darkop.py

--- darkflow/dark/darkop.py
+++ darkflow/dark/darkop.py
@@ -52,7 +52,8 @@ darkops = {
     'reorg': reorg_layer,
     'conv-select': conv_select_layer,
     'conv-extract': conv_extract_layer,
-    'extract': extract_layer
+    'extract': extract_layer,
+    'rnn': rnn_layer
 }
 
 def create_darkop(ltype, num, *args):
