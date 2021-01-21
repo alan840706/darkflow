ac5422f2a2dccac8b7cfb2249cab276ddef76289
alan840706 <58114247+alan840706@users.noreply.github.com>
2021-01-21 18:18:00 +0800

    Update process.py

--- darkflow/utils/process.py
+++ darkflow/utils/process.py
@@ -330,9 +330,9 @@ def cfg_yielder(model, binary):
 			out = d['output']
 			hidden = d['hidden']
 			activation = d.get('activation', 'logistic')
-			yield ['rnn', i,d['output'], activation]
+			yield ['rnn', i, l, d['output'], activation]
 			if activation != 'linear': yield [activation, i]
-			l=ipt
+			l = d['output']
 		#-----------------------------------------------------
 		else:
 			exit('Layer {} not implemented'.format(d['type']))
