4e46d70bc6a40388b3402bb484b97172aebe3c8e
alan840706 <58114247+alan840706@users.noreply.github.com>
2021-01-21 14:56:42 +0800

    Update rnn.py

--- darkflow/dark/rnn.py
+++ darkflow/dark/rnn.py
@@ -2,42 +2,22 @@ from .layer import Layer
 import numpy as np
 
 class rnn_layer(Layer):
-    def setup(self, ksize, c, n, stride, 
-              pad, batch_norm, activation,groups):
-        self.batch_norm = bool(batch_norm)
+    def setup(self, input_size, 
+              output_size, activation):
         self.activation = activation
-        self.stride = stride
-        self.ksize = ksize
-        self.pad = pad
-        self.groups = groups
-        if groups>1:
-            self.dnshape = [1,c , ksize, ksize] # darknet shape
-            self.wshape = dict({
-                'biases': [n], 
-                'kernel': [ksize, ksize, c, 1]
-            })
-        else:   
-            self.dnshape = [n,c , ksize, ksize] # darknet shape
-            self.wshape = dict({
-                'biases': [n], 
-                'kernel': [ksize, ksize, c, n]
-            })
-        if self.batch_norm:
-            self.wshape.update({
-                'moving_variance'  : [n], 
-                'moving_mean': [n], 
-                'gamma' : [n]
-            })
-            self.h['is_training'] = {
-                'feed': True,
-                'dfault': False,
-                'shape': ()
-            }
+        self.inp = input_size
+        self.out = output_size
+        self.wshape = {
+            'biases': [self.out],
+            'weights': [self.inp, self.out]
+        }
 
-    def finalize(self, _):
-        """deal with darknet"""
-        kernel = self.w['kernel']
-        if kernel is None: return
-        kernel = kernel.reshape(self.dnshape)
-        kernel = kernel.transpose([2,3,1,0])
-        self.w['kernel'] = kernel
+    def finalize(self, transpose):
+        weights = self.w['weights']
+        if weights is None: return
+        shp = self.wshape['weights']
+        if not transpose:
+            weights = weights.reshape(shp[::-1])
+            weights = weights.transpose([1,0])
+        else: weights = weights.reshape(shp)
+        self.w['weights'] = weights
